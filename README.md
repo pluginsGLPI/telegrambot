5f65a3f92b220e4ef07c1eb6be195cb05f22d289
Filipe Menezes <filipepmenezes@gmail.com>
2017-10-17 19:48:36 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TelegramBot GLPI plugin
+
+Add your plugin description here.
+
+## Contributing
+
+* Open a ticket for each bug/feature so it can be discussed
+* Follow [development guidelines](http://glpi-developer-documentation.readthedocs.io/en/latest/plugins/index.html)
+* Refer to [GitFlow](http://git-flow.readthedocs.io/) process for branching
+* Work on a new branch on your own fork
+* Open a PR that will be reviewed by a developer
---
+++ hook.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ -------------------------------------------------------------------------
+ TelegramBot plugin for GLPI
+ Copyright (C) 2017 by the TelegramBot Development Team.
+
+ https://github.com/pluginsGLPI/telegrambot
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of TelegramBot.
+
+ TelegramBot is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ TelegramBot is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with TelegramBot. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
+/**
+ * Plugin install process
+ *
+ * @return boolean
+ */
+function plugin_telegrambot_install() {
+   return true;
+}
+
+/**
+ * Plugin uninstall process
+ *
+ * @return boolean
+ */
+function plugin_telegrambot_uninstall() {
+   return true;
+}
